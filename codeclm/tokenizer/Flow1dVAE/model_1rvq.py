43425a9c48fdaefb0ac2fb4cdd9cd1cdcde660d1
FurkanGozukara <furkangozukara@gmail.com>
2026-02-20 01:14:21 +0300

    7.5

--- codeclm/tokenizer/Flow1dVAE/model_1rvq.py
+++ codeclm/tokenizer/Flow1dVAE/model_1rvq.py
@@ -298,14 +298,14 @@ class PromptCondAudioDiffusion(nn.Module):
         base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
         self.bestrq = load_model(
             model_dir=os.path.join(base_dir, 'codeclm/tokenizer/Flow1dVAE/our_MERT_BESTRQ/mert_fairseq'),
-            checkpoint_dir=os.path.join(base_dir, 'ckpt/ckpt/encode-s12k.pt'),
+            checkpoint_dir=os.path.join(base_dir, 'ckpt', 'encode-s12k.pt'),
         )
         self.rsq48tobestrq = torchaudio.transforms.Resample(48000, 24000)
         self.rsq48tohubert = torchaudio.transforms.Resample(48000, 16000)
         for v in self.bestrq.parameters():v.requires_grad = False
         self.rvq_bestrq_emb = ResidualVectorQuantize(input_dim = 1024, n_codebooks = 1, codebook_size = 16_384, codebook_dim = 32, quantizer_dropout = 0.0, stale_tolerance=200)
         for v in self.rvq_bestrq_emb.parameters():v.requires_grad = False
-        self.hubert = HubertModelWithFinalProj.from_pretrained(os.path.join(base_dir, 'ckpt', 'ckpt', 'models--lengyue233--content-vec-best', 'snapshots', 'c0b9ba13db21beaa4053faae94c102ebe326fd68'))
+        self.hubert = HubertModelWithFinalProj.from_pretrained(os.path.join(base_dir, 'ckpt', 'models--lengyue233--content-vec-best', 'snapshots', 'c0b9ba13db21beaa4053faae94c102ebe326fd68'))
         for v in self.hubert.parameters():v.requires_grad = False
         self.zero_cond_embedding1 = nn.Parameter(torch.randn(32*32,))
         # self.xvecmodel = XVECModel()
